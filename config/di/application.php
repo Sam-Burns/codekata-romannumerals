5effeaf3dd915bad5c960508a66de7a2b19f330d
Sam Burns <sam.burns.uk@gmail.com>
2016-01-06 16:58:21 +0000

    Tidy-up

--- config/di/application.php
+++ config/di/application.php
@@ -1,22 +1,24 @@
 <?php
 
+use Interop\Container\ContainerInterface as Container;
+
 return [
 
     'codekata.application.application' =>
-        function (Interop\Container\ContainerInterface $container) {
+        function (Container $container) {
             $consoleAppolication = $container->get('codekata.application.symfony-console-application');
             $helloCommand        = $container->get('codekata.application.hello-command');
             return new \CodeKata\RomanNumerals\Application\Application($consoleAppolication, $helloCommand);
         },
 
     'codekata.application.hello-command' =>
-        function (Interop\Container\ContainerInterface $container) {
+        function (Container $container) {
             $helloService = $container->get('codekata.domain.hello-service');
             return new \CodeKata\RomanNumerals\Application\HelloCommand($helloService);
         },
 
     'codekata.application.symfony-console-application' =>
-        function (Interop\Container\ContainerInterface $container) {
+        function (Container $container) {
             return new \Symfony\Component\Console\Application();
         },
 
--- config/di/domain.php
+++ config/di/domain.php
@@ -1,9 +1,11 @@
 <?php
 
+use Interop\Container\ContainerInterface as Container;
+
 return [
 
     'codekata.domain.hello-service' =>
-        function (Interop\Container\ContainerInterface $container) {
+        function (Container $container) {
           return new \CodeKata\RomanNumerals\Domain\HelloService();
         },
 
