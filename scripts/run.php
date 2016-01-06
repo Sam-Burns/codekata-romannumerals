bc9377181f2ec9086caca91e449c801d17688eb9
Sam Burns <sam.burns.uk@gmail.com>
2016-01-06 17:55:39 +0000

    Updating DI container

--- scripts/run.php
+++ scripts/run.php
@@ -3,8 +3,8 @@ require_once dirname(__DIR__) . '/src/bootstrap.php';
 
 $diContainer = new \UltraLite\Container\Container();
 
-$diContainer->setServiceFactories(require APPLICATION_ROOT_DIR . '/config/di/application.php');
-$diContainer->setServiceFactories(require APPLICATION_ROOT_DIR . '/config/di/domain.php');
+$diContainer->configureFromFile(APPLICATION_ROOT_DIR . '/config/di/application.php');
+$diContainer->configureFromFile(APPLICATION_ROOT_DIR . '/config/di/domain.php');
 
 $application = $diContainer->get('codekata.application.application');
 
--- tests/phpunit/CodeKata/Test/RomanNumerals/DiConfigTest.php
+++ tests/phpunit/CodeKata/Test/RomanNumerals/DiConfigTest.php
@@ -7,18 +7,15 @@ use Interop\Container\ContainerInterface;
 
 class DiConfigTest extends TestCase
 {
-    /** @var ContainerInterface */
-    private $diContainer;
-
     public function testRetrievingApplicationObjectGraph()
     {
         // ARRANGE
-        $this->diContainer = new Container();
-        $this->diContainer->setServiceFactories(require APPLICATION_ROOT_DIR . '/config/di/application.php');
-        $this->diContainer->setServiceFactories(require APPLICATION_ROOT_DIR . '/config/di/domain.php');
+        $diContainer = new Container();
+        $diContainer->configureFromFile(APPLICATION_ROOT_DIR . '/config/di/application.php');
+        $diContainer->configureFromFile(APPLICATION_ROOT_DIR . '/config/di/domain.php');
 
         // ACT
-        $result = $this->diContainer->get('codekata.application.application');
+        $result = $diContainer->get('codekata.application.application');
 
         // ASSERT
         $this->assertInstanceOf('\CodeKata\RomanNumerals\Application\Application', $result);
